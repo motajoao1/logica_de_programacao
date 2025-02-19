c89e315734bd9553fbcbb52d40eab89460dfeec2
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2025-02-19 19:51:26 +0000

    salve

--- 19.02 exercicios/if_else4.py
+++ 19.02 exercicios/if_else4.py
@@ -12,7 +12,15 @@ soma = numero + numero2
 produto = numero * numero2
 
 if numero > numero2:
-  print(media, soma, produto, numero2, numero)
+  print("Esta é a média: ", media)
+  print("Esta é a soma: ", soma)
+  print("Este é o produto: ", produto)
+  print("Este é o menor valor: ", numero2)
+  print("Este é o menor valor: ", numero)
 if numero < numero2:
-  print(media, soma, produto, numero, numero2)
+  print("Esta é a média: ", media)
+  print("Esta é a soma: ", soma)
+  print("Este é o produto: ", produto)
+  print("Este é o menor valor: ", numero)
+  print("Este é o menor valor: ", numero2)
 
