946bfbe6a028f8fd5c1001cac8460473b8b8e06c
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2025-03-31 14:41:18 -0300

    exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -4,6 +4,8 @@ os.system ("cls || clear")
 salarios = 0
 i = 0
 j = 0
+maior_idade = 0
+menor_idade = 9999
 
 while True:
     print( """
@@ -21,11 +23,14 @@ while True:
             salarios += salario
             i += 1
             total = salarios / i
+            maior_idade = max(maior_idade, idd)
+            menor_idade = min(menor_idade, idd)
             if sexo == 'F' and salario > 5000:
                 j += 1
         case 2:
             print(f"A média de salário é: {total}")
-            print(f"A quantidade de mulheres com salário maior que R$5.000,00 é: {j}")
+            print(f"A menor idade é: {menor_idade} e a maior idade é: {maior_idade}")
+            print(f"A quantidade de mulheres com salário maior que R$5K é: {j}")
         case 3:
             print("FIM DO PROGRAMA")
             break
