9399100905bbdf038583ff3b88798d4a11a06664
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2025-05-06 04:29:29 -0300

    atvididade2.py

--- dataclass/atvididade2.py
+++ dataclass/atvididade2.py
@@ -1,6 +1,7 @@
 import os
+os.system("cls || clear")
 from dataclasses import dataclass
-os.system('cls || clear')
+
 
 def limpar():
     os.system("cls || clear")
@@ -11,29 +12,37 @@ class Usuario:
     data_nascimento: float
     rg: float
     cpf: float
-
+    
     def exibir(self):
-        print(f"\nNome: {self.nome} \nAutor:{self.data_nascimento} \nCategoria: {self.rg} \nPreço:{self.cpf} ")
+        print(f"\nNome: {self.nome}\nData de nascimento: {self.data_nascimento}\nRG: {self.rg}\nCPF: {self.cpf}")
+        
+QUANTIDADE =  5
+lista = []
 
-Quantidade = 5
-lista_livros = []
-
-for i in range(Quantidade):
-    Usuario = Usuario(
-        nome=input("Digite o nome do usuário: "),
-        data_nascimento=input("Digite seu nascimento: ")
-        rg=input("Informe seu rg: ")
-        cpf=input("Informe seu cpf: ")
+for i in range(QUANTIDADE):
+    usuario = Usuario(
+    nome = input("Digite o nome do usuário: "),
+    data_nascimento = input("Digite sua data de nascimento: "),
+    rg =input("Informe seu RG: "),
+    cpf = input("informe seu CPF: ")
     )
-    limpar()
+    limpar()  
+   
 
-lista_livros.append(livros)
+lista.append(usuario)
 
-nome_arquivo = "olê.txt"
-with open (nome_arquivo, "a", encoding="utf-8") as arquivo_livro:
-    for livro in lista_livros:
-        arquivo_livro.write(f"\nTítulo do livro: {livro.titulo}\Ano de publicação: {livro.ano}\Nome do autor: {livro.autor.nome} \Biografia do autor: {livro.autor.biografia}")
-print(f"\t\nSalvando...")
+nome_arquivo = "Funcionarios.txt"
+with open(nome_arquivo, "a", encoding="utf-8") as arquivo_usuario:
+    for livro in lista:
+        arquivo_usuario.write(f"Nome: {usuario.nome}\nData de nascimento: {usuario.data_nascimento}\nRG do usuário: {usuario.rg}\nCPF do usuário: {usuario.cpf}")
+
+print("*Dados salvos com sucesso*")
+
+nome_arquivo = "Funcionarios.txt"
+with open(nome_arquivo, "a", encoding="utf-8") as arquivo_usuario:
+    for usuario in lista:
+        arquivo_usuario.write(f"\nNome do Usuário: {usuario.nome}\nData de nascimento do usuário: {usuario.data_nascimento}\nRG do usuario: {usuario.rg}\nCPF do usuário: {usuario.pf}")
+print(f"\t\nSalvando . .  .")
 
 try:
     with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
@@ -42,6 +51,3 @@ try:
             print(f"{linha.strip()}")
 except FileNotFoundError:
     print("Arquivo não encontrado")
-
-
-print ("\n= Dados Salvos = ")
