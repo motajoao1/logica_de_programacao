6990d2855b2c287d8bfa36ea8b1d4f0611a4b334
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2025-05-29 10:15:56 -0300

    atv5.js

---
+++ front-end2/javascript/atv5.js
@@ -0,0 +1,14 @@
+const readline = require('readline-sync')
+
+let numero; 
+    numero = parseInt(readline.question('Digite um numero: '))
+
+
+
+if (numero >=100) {
+    console.log(`Número alto !`)
+    }
+
+else {  
+    console.log(`Número baixo.`)
+    } ;
